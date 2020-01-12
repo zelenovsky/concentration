2716c55f29cc34e45be6f8ab03794436408ffcbd
Artem Zelenov <deeloyy@gmail.com>
2020-01-12 20:35:33 +0600

    first commit

---
+++ src/theme/theme.js
@@ -0,0 +1,10 @@
+export default {
+  colors: {
+    black: '#222',
+    white: '#fff'
+  },
+  space: ['0', '4px', '8px', '16px', '24px', '32px', '40px', '48px', '64px', '80px', '96px', '112px', '128px'],
+  sizes: ['544px', '768px', '1012px', '1280px'],
+  breakpoints: ['544px', '768px', '1012px', '1280px'],
+  radii: ['0', '3px', '6px'],
+}
